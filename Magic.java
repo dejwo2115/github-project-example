03c30a6c50ca3b19b85d8e1ad6f471e669f59c52
dawid <dawideq195@gmail.com>
2023-11-04 13:50:40 +0100

    commiting magic class

---
+++ Magic.java
@@ -0,0 +1,6 @@
+public class Magic {
+    public static void main(String[] args) {
+        String letsTheMagicBegin = "Lets the magic begin";
+        System.out.println(letsTheMagicBegin);
+    }
+}
